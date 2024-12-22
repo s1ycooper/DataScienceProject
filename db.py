c17ccc351cb650f0f5610243ad35c8c6314fae5f
s1ycooper <yanikmanish@gmail.com>
2024-12-22 12:13:27 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 # Load datasets
 raw_data = pd.read_csv("https://raw.githubusercontent.com/s1ycooper/DataScienceProject/refs/heads/main/synthetic_insurance_data.csv")
-preprocessed_data = pd.read_csv("synthetic_insurance_data2.csv")
+preprocessed_data = pd.read_csv("https://raw.githubusercontent.com/s1ycooper/DataScienceProject/refs/heads/main/synthetic_insurance_data2.csv")
 
 # Page configuration
 st.set_page_config(page_title="Insurance Data Comparison", layout="wide")
